5aab409b496289eee30a5c214d3d13b84aad89be
Paul Lockwood <plockwood@shipjunction.com>
2018-01-05 12:30:48 -0700

    Remove dependency on npm package custom-protocol-detection  - Moved his/her/their single .js file into this codebase  - Played around with how to include it  - First used the scripts section in .angular-cli.json (seems to just paste the script into the main bundle.js)  - FInally embedded the protocolcheck.js file in a folder called 3rd-party  - Looks like we need to use require() to import unless more work is done (presumably to make it a real ES6 module?)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import protocolCheck from 'custom-protocol-detection';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+// import protocolCheck from 'custom-protocol-detection';
+// const protocolCheck = require('custom-protocol-detection');
+
+declare const require: any; // Squash error: Cannot find name 'require'
+const protocolCheck = require('3rd-party/protocolcheck');
+
+// import * as lowerCase from 'lowerCase';
+// const lowerCase = require('lower-case');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app2';
+
+  ngOnInit() {
+    // const org = 'UPPER-CASE';
+    // const xyzzy = lowerCase(org);
+    // tslint:disable-next-line:no-console
+    // console.info(org + '  - ' + xyzzy);
+  }
 
   testValid() {
     this.callProtocolCheck('qz:launch');
@@ -19,8 +35,10 @@ export class AppComponent {
 
   callProtocolCheck(customUri) {
 
+
     console.log(customUri);
 
+    // (window as any).
     protocolCheck(customUri,
       () => {
         console.log('This browser does not support the protocol');
